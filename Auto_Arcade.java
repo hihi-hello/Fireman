44f0f90986723d08bebb79b5a719b36d574ba311
hihi-hello <65531081+hihi-hello@users.noreply.github.com>
2021-06-08 21:54:35 +0900

    Create Auto_Arcade.java

---
+++ Auto_Arcade.java
@@ -0,0 +1,22 @@
+/*작성자 : 김영현
+  내용 : auto arcade 관련 정보 저장 */
+
+package com.example.fireoperationmap;
+
+public class Auto_Arcade{
+    private float x;
+    private float y;
+    private int num;
+
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public int getNum() { return num; }
+
+    public Auto_Arcade(){}
+}
