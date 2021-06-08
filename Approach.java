97b62de7edd862febd0f9625eade629a8d83a5b7
hihi-hello <65531081+hihi-hello@users.noreply.github.com>
2021-06-08 21:55:58 +0900

    Create Approach.java

---
+++ Approach.java
@@ -0,0 +1,32 @@
+/*작성자 : 김영현
+  내용 : 진입로 관련 정보들 저장 */
+
+package com.example.fireoperationmap;
+
+public class Approach{
+    private String name;
+    private String address;
+    private int num;
+    private float x;
+    private float y;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public int getNum() { return num; }
+
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public Approach(){}
+}
