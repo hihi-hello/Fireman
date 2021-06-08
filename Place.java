92209f5529d2376709d9087c6d8d43b2ac6018ff
hihi-hello <65531081+hihi-hello@users.noreply.github.com>
2021-06-08 21:56:49 +0900

    Create Place.java

---
+++ Place.java
@@ -0,0 +1,25 @@
+/*작성자 : 김영현
+* 내용 : 위치 정보 저장 */
+
+package com.example.fireoperationmap;
+
+public class Place {
+    private float x;
+    private float y;
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
+}
